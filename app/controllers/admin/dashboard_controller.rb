eaf7e6e4f0b7a7bb0260524361b1e5795b397d53
jrivera604 <joshrivera91@hotmail.com>
2023-05-03 03:32:42 +0000

    add about us page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,9 @@
-class Admin::DashboardController < ApplicationController
-  include HttpAuthConcern
 
+
+class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: [:index]
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  include HttpAuthConcern
 
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: [:index]
 
