ae118128807d5d48bc7b503310f4b1045900f24c
jrivera604 <joshrivera91@hotmail.com>
2023-05-03 02:53:35 +0000

    add admin authentication

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  include HttpAuthConcern
+
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: [:index]
   def show
   end
 end
