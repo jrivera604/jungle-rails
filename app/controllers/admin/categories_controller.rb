ebbe1be417a3e3a06a0205306d704ead5e095321
jrivera604 <joshrivera91@hotmail.com>
2023-05-03 20:49:47 +0000

    add category create and edit

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,37 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to admin_categories_path, notice: 'Category created successfully'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: 'Category updated successfully'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
